ba93d6bd2560f3f00ddb1c56393a2b8f5c45883e
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-04-22 22:51:48 +0900

    virtualenv

--- scripts/prezto.sh
+++ scripts/prezto.sh
@@ -4,6 +4,6 @@ setopt EXTENDED_GLOB
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/layout/prompt_agnoster_setup ~/.prezto/modules/prompt/functions/prompt_agnoster_setup
+ln -sfn ~/dotfiles/layout/prompt_agnoster_setup ~/.prezto/modules/prompt/functions/prompt_agnoster_setup
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 source ~/.zshrc
--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -3,3 +3,4 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 source ~/.zshrc
 pyenv install 3.6.0
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
