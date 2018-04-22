4ec00732b5aa7aedd4540ef399d66cc9547d954c
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-04-22 20:46:41 +0900

    ln

--- scripts/prezto.sh
+++ scripts/prezto.sh
@@ -2,7 +2,7 @@ cd `dirname $0`
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 ln -sf ../.zshrc ~/.zshrc
 ln -sf ../.zpreztorc ~/.zpreztorc
