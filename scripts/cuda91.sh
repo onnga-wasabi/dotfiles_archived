fcf5e65dd35313bedfc3a866959a329814ce6763
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-04-30 00:12:36 +0900

    cuda91

--- scripts/cuda91.sh
+++ scripts/cuda91.sh
@@ -6,9 +6,9 @@ if [ ! -e cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64 ];then
     wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64
 fi
 echo [Attention] install patch 1
-#sudo dpkg -i cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64
-#sudo apt-get update
-#sudo apt-get -y install cuda
+sudo dpkg -i cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64
+sudo apt-get update
+sudo apt-get -y install cuda
 
 # patch 1
 if [ ! -e cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-1_1.0-1_amd64 ];then
@@ -16,7 +16,7 @@ if [ ! -e cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-1_1.0-1_amd64
     wget https://developer.nvidia.com/compute/cuda/9.1/prod/patches/1/cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-1_1.0-1_amd64
 fi
 echo [Attention] install patch 1
-#sudo dpkg -i cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-1_1.0-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-1_1.0-1_amd64
 
 # patch 2
 if [ ! -e cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64 ];then
@@ -24,7 +24,7 @@ if [ ! -e cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64 ];then
     wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/2/cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64
 fi
 echo [Attention] install patch 2
-#sudo dpkg -i cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64
 
 # patch 3
 if [ ! -e cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-3_1.0-1_amd64 ];then
@@ -32,4 +32,4 @@ if [ ! -e cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-3_1.0-1_amd64
     wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/3/cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-3_1.0-1_amd64
 fi
 echo [Attention] install patch 3
-#sudo dpkg -i cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64
