c4efc546fb8f8e3fea690e226c0aaaa5900b21f4
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-08-02 14:49:21 +0900

    pyenv root

--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -1,6 +1,6 @@
 cd `dirname $0`
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 source ~/.zshrc
 pyenv install 3.6.0
